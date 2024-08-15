9e249931ae38111e04afec21ad37ec2360c1ff1e
nicorm2000 <nicorm2000@gmail.com>
2024-08-15 19:07:02 -0300

    Fixed issues and added force state

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -25,19 +25,21 @@ public class Agent : MonoBehaviour
 
     private void Start()
     {
-        fsm = new FSM(Enum.GetValues(typeof(Behaviour)).Length, Enum.GetValues(typeof(Flags)).Length);
+        fsm = new FSM(Enum.GetValues(typeof(Behaviours)).Length, Enum.GetValues(typeof(Flags)).Length);
 
         //Add states and transitions
         fsm.AddBehaviour<ChaseState>((int)Behaviours.Chase,
             onTickParameters: () => { return new object[] { transform, target, speed, explodeDistance, lostDistance }; });
+        fsm.AddBehaviour<ChaseState>((int)Behaviours.Patrol,
+            onTickParameters: () => { return new object[] { transform, wayPoint1, wayPoint2, target, speed, chaseDistance }; });
+        fsm.AddBehaviour<ChaseState>((int)Behaviours.Explode);
+
+
+        fsm.SetTransition((int)Behaviours.Patrol, (int)Flags.OnTargetNear, (int)Behaviours.Chase);
         fsm.SetTransition((int)Behaviours.Chase, (int)Flags.OnTargetReach, (int)Behaviours.Explode);
         fsm.SetTransition((int)Behaviours.Chase, (int)Flags.OnTargetLost, (int)Behaviours.Patrol);
 
-        fsm.AddBehaviour<ChaseState>((int)Behaviours.Patrol,
-            onTickParameters: () => { return new object[] { transform, wayPoint1, wayPoint2, target, speed, chaseDistance }; });
-        fsm.SetTransition((int)Behaviours.Patrol, (int)Flags.OnTargetNear, (int)Behaviours.Chase);
-
-        fsm.AddBehaviour<ChaseState>((int)Behaviours.Explode);
+        fsm.ForceState((int)Behaviours.Patrol);
     }
 
     private void Update()
