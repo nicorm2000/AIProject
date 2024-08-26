3abfd7ed8ff3d19c78c5be94d02ae92115f11802
nicorm2000 <nicorm2000@gmail.com>
2024-08-26 11:13:57 -0300

    Added astar and dijkstra

--- Assets/Scripts/Pathfinder/AStarPathfinder.cs
+++ Assets/Scripts/Pathfinder/AStarPathfinder.cs
@@ -1,35 +1,113 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Utils;
 
-public class AStarPathfinder<NodeType> : Pathfinder<NodeType> where NodeType : INode
+namespace Pathfinder
 {
-    protected override int Distance(NodeType A, NodeType B)
+    public struct Transition<NodeType>
     {
-        //int distance = 0;
-        //
-        //distance = Mathf.Abs(Mathf.Sqrt((A as INode(int x, int y) * A) + (B * B)));
-        //
-        //return distance;
-        throw new System.NotImplementedException();
+        public NodeType to;
+        public int cost;
+        public int distance;
     }
 
-    protected override ICollection<NodeType> GetNeighbours(NodeType node)
+    public class AStarPathfinder<NodeType> : Pathfinder<NodeType> where NodeType : INode<Vec2Int>, INode, new()
     {
-        throw new System.NotImplementedException();
-    }
+        public Dictionary<NodeType, List<Transition<NodeType>>> transitions =
+            new Dictionary<NodeType, List<Transition<NodeType>>>();
 
-    protected override bool IsBlocked(NodeType node)
-    {
-        return node.IsBlocked();
-    }
 
-    protected override int MoveToNeighbourCost(NodeType A, NodeType B)
-    {
-        throw new System.NotImplementedException();
-    }
+        public AStarPathfinder(Vector2IntGraph<NodeType> graph, int minCost = -1, int maxCost = 3)
+        {
+            this.Graph = graph;
 
-    protected override bool NodesEquals(NodeType A, NodeType B)
-    {
-        throw new System.NotImplementedException();
+            graph.nodes.ForEach(node =>
+            {
+                List<Transition<NodeType>> transitionsList = new List<Transition<NodeType>>();
+
+                List<NodeType> neighbors = GetNeighbors(node) as List<NodeType>;
+                neighbors?.ForEach(neighbor =>
+                {
+                    transitionsList.Add(new Transition<NodeType>
+                    {
+                        to = neighbor,
+                        cost = RandomNumberGenerator.GetInt32(minCost, maxCost),
+                        distance = Distance(node, neighbor)
+                    });
+                });
+                transitions.Add(node, transitionsList);
+            });
+        }
+
+        public AStarPathfinder(int sizeX, int sizeY)
+        {
+            Graph = new Vector2IntGraph<NodeType>(sizeX, sizeY);
+        }
+
+        protected override int Distance(NodeType A, NodeType B)
+        {
+            int distance = 0;
+
+            var aCoor = (A).GetCoordinate();
+            var bCoor = (B).GetCoordinate();
+
+            distance += Math.Abs(aCoor.x - bCoor.x);
+            distance += Math.Abs(aCoor.y - bCoor.y);
+
+            return distance;
+        }
+
+        protected override ICollection<NodeType> GetNeighbors(NodeType node)
+        {
+            List<NodeType> neighbors = new List<NodeType>();
+
+            var nodeCoor = node.GetCoordinate();
+
+            Graph.nodes.ForEach(neighbor =>
+            {
+                var neighborCoor = neighbor.GetCoordinate();
+                if ((neighborCoor.x == nodeCoor.x && Math.Abs(neighborCoor.y - nodeCoor.y) == 1) ||
+                    (neighborCoor.y == nodeCoor.y && Math.Abs(neighborCoor.x - nodeCoor.x) == 1) ||
+                    (Math.Abs(neighborCoor.y - nodeCoor.y) == 1 && Math.Abs(neighborCoor.x - nodeCoor.x) == 1))
+                {
+                    neighbors.Add(neighbor);
+                }
+            });
+
+            return neighbors;
+        }
+
+        protected override bool IsBlocked(NodeType node)
+        {
+            return node.IsBlocked();
+        }
+
+        protected override int MoveToNeighborCost(NodeType A, NodeType B)
+        {
+            if (!GetNeighbors(A).Contains(B))
+            {
+                throw new InvalidOperationException("B node has to be a neighbor.");
+            }
+
+            int cost = 0;
+
+            transitions.TryGetValue(A, out List<Transition<NodeType>> transition);
+
+            transition?.ForEach(t =>
+            {
+                if (NodesEquals(t.to, B))
+                {
+                    cost = t.cost;
+                }
+            });
+
+            return cost;
+        }
+
+        protected override bool NodesEquals(NodeType A, NodeType B)
+        {
+            return A.GetCoordinate().x == B.GetCoordinate().x && A.GetCoordinate().y == B.GetCoordinate().y;
+        }
     }
 }
--- Assets/Scripts/Pathfinder/Traveler.cs
+++ Assets/Scripts/Pathfinder/Traveler.cs
@@ -10,7 +10,7 @@ namespace Pathfinder
     {
         [FormerlySerializedAs("graphView")] public GraphView graphView;
 
-        //private AStarPathfinder<Node<Vec2Int>> Pathfinder;
+        private AStarPathfinder<Node<Vec2Int>> Pathfinder;
         //private DijkstraPathfinder<Node<Vector2Int>> Pathfinder;
         //private DepthFirstPathfinder<Node<Vector2Int>> Pathfinder;
         //private BreadthPathfinder<Node<Vec2Int>> Pathfinder;
@@ -20,21 +20,21 @@ namespace Pathfinder
 
         void Start()
         {
-            //Pathfinder = new AStarPathfinder<Node<Vec2Int>>(graphView.Graph);
-            //startNode = new Node<Vec2Int>();
-            //startNode.SetCoordinate(new Vec2Int(Random.Range(0, 10), Random.Range(0, 10)));
-            //
-            //destinationNode = new Node<Vec2Int>();
-            //destinationNode.SetCoordinate(new Vec2Int(Random.Range(0, 10), Random.Range(0, 10)));
-            //
-            //List<Node<Vec2Int>> path = Pathfinder.FindPath(startNode, destinationNode);
-            //
-            //graphView.Transitions = Pathfinder.transitions;
-            //graphView.startNode = startNode;
-            //graphView.destinationNode = destinationNode;
-            //graphView.path = path;
-            //
-            //StartCoroutine(Move(path));
+            Pathfinder = new AStarPathfinder<Node<Vec2Int>>(graphView.Graph);
+            startNode = new Node<Vec2Int>();
+            startNode.SetCoordinate(new Vec2Int(Random.Range(0, 10), Random.Range(0, 10)));
+            
+            destinationNode = new Node<Vec2Int>();
+            destinationNode.SetCoordinate(new Vec2Int(Random.Range(0, 10), Random.Range(0, 10)));
+            
+            List<Node<Vec2Int>> path = Pathfinder.FindPath(startNode, destinationNode);
+            
+            graphView.Transitions = Pathfinder.transitions;
+            graphView.startNode = startNode;
+            graphView.destinationNode = destinationNode;
+            graphView.path = path;
+            
+            StartCoroutine(Move(path));
         }
 
         private IEnumerator Move(List<Node<Vec2Int>> path)
