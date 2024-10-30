be31692e8b9e75b069a4a7f63566d6344559e603
nicorm2000 <nicorm2000@gmail.com>
2024-10-30 20:26:48 -0300

    Updated flocking

--- Assets/Scripts/Flocking/Boid.cs
+++ Assets/Scripts/Flocking/Boid.cs
@@ -1,46 +1,45 @@
 using System;
 using UnityEngine;
 
-namespace Flocking
+public class Boid : MonoBehaviour
 {
-    public class Boid : MonoBehaviour
+    public float speed = 2.5f;
+    public float turnSpeed = 5f;
+    public float detectionRadious = 3.0f;
+    public float alignmentOffset = 0.0f;
+    public float cohesionOffset = 0.0f;
+    public float separationOffset = 0.0f;
+    public float directionOffset = 0.0f;
+
+    private Func<Boid, Vector3> Alignment;
+    private Func<Boid, Vector3> Cohesion;
+    private Func<Boid, Vector3> Separation;
+    private Func<Boid, Vector3> Direction;
+
+    public void Init(Func<Boid, Vector3> Alignment,
+                     Func<Boid, Vector3> Cohesion,
+                     Func<Boid, Vector3> Separation,
+                     Func<Boid, Vector3> Direction)
     {
-        public float speed = 2.5f;
-        public float turnSpeed = 5f;
-        public float detectionRadius = 3.0f;
+        this.Alignment = Alignment;
+        this.Cohesion = Cohesion;
+        this.Separation = Separation;
+        this.Direction = Direction;
+    }
 
-        public static float alignmentWeight = 1.0f;
-        public static float cohesionWeight = 1.0f;
-        public static float separationWeight = 1.0f;
-        public static float directionWeight = 1.0f;
+    private void Update()
+    {
+        Vector3 desiredDirection = ACS();
+        transform.forward = Vector3.Lerp(transform.forward, desiredDirection, turnSpeed * Time.deltaTime);
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
 
-        private Func<Boid, Vector3> Alignment;
-        private Func<Boid, Vector3> Cohesion;
-        private Func<Boid, Vector3> Separation;
-        private Func<Boid, Vector3> Direction;
-
-        public void Init(Func<Boid, Vector3> Alignment,
-            Func<Boid, Vector3> Cohesion,
-            Func<Boid, Vector3> Separation,
-            Func<Boid, Vector3> Direction)
-        {
-            this.Alignment = Alignment;
-            this.Cohesion = Cohesion;
-            this.Separation = Separation;
-            this.Direction = Direction;
-        }
-
-        private void Update()
-        {
-            transform.position += transform.forward * (speed * Time.deltaTime);
-            transform.forward = Vector3.Lerp(transform.forward, ACS(), turnSpeed * Time.deltaTime);
-        }
-
-        public Vector3 ACS()
-        {
-            Vector3 ACS = Alignment(this) * alignmentWeight + Cohesion(this) * cohesionWeight +
-                          Separation(this) * separationWeight + Direction(this) * directionWeight;
-            return ACS.normalized;
-        }
+    public Vector3 ACS()
+    {
+        Vector3 ACS = (Alignment(this) * alignmentOffset) +
+                      (Cohesion(this) * cohesionOffset) +
+                      (Separation(this) * separationOffset) +
+                      (Direction(this) * directionOffset);
+        return ACS.normalized;
     }
 }
--- Assets/Scripts/Flocking/FlockingManager.cs
+++ Assets/Scripts/Flocking/FlockingManager.cs
@@ -1,107 +1,91 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
-namespace Flocking
+public class FlockingManager : MonoBehaviour
 {
-    public class FlockingManager : MonoBehaviour
+    public Transform target;
+    public int boidCount = 50;
+    public Boid boidPrefab;
+    private List<Boid> boids = new List<Boid>();
+
+    private void Start()
     {
-        public Transform target;
-        public int boidCount = 50;
-        public Boid boidPrefab;
-        [SerializeField] private float alignmentWeight = 1.0f;
-        [SerializeField] private float cohesionWeight = 1.0f;
-        [SerializeField] private float separationWeight = 1.0f;
-        [SerializeField] private float directionWeight = 1.0f;
-
-        private List<Boid> boids = new List<Boid>();
-
-        private void Start()
+        for (int i = 0; i < boidCount; i++)
         {
-            for (int i = 0; i < boidCount; i++)
-            {
-                GameObject boidGO = Instantiate(boidPrefab.gameObject,
-                    new Vector3(Random.Range(-10, 10), Random.Range(-10, 10)), Quaternion.identity);
+            GameObject boidGO = Instantiate(boidPrefab.gameObject,
+                new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0), Quaternion.identity);
 
-                Boid boid = boidGO.GetComponent<Boid>();
-                boid.Init(Alignment, Cohesion, Separation, Direction);
-                boids.Add(boid);
+            Boid boid = boidGO.GetComponent<Boid>();
+            boid.Init(Alignment, Cohesion, Separation, Direction);
+
+            boid.alignmentOffset = 1.0f;
+            boid.cohesionOffset = 1.5f;
+            boid.separationOffset = 2.0f;
+            boid.directionOffset = 2.0f;
+
+            boids.Add(boid);
+        }
+    }
+
+    public Vector3 Alignment(Boid boid)
+    {
+        List<Boid> insideRadiusBoids = GetBoidsInsideRadius(boid);
+        if (insideRadiusBoids.Count == 0) return boid.transform.up;
+
+        Vector3 avg = Vector3.zero;
+        foreach (Boid b in insideRadiusBoids)
+        {
+            avg += b.transform.up;
+        }
+        avg /= insideRadiusBoids.Count;
+        return avg.normalized;
+    }
+
+    public Vector3 Cohesion(Boid boid)
+    {
+        List<Boid> insideRadiusBoids = GetBoidsInsideRadius(boid);
+        if (insideRadiusBoids.Count == 0) return Vector3.zero;
+
+        Vector3 avg = Vector3.zero;
+        foreach (Boid b in insideRadiusBoids)
+        {
+            avg += b.transform.position;
+        }
+        avg /= insideRadiusBoids.Count;
+        return (avg - boid.transform.position).normalized;
+    }
+
+    public Vector3 Separation(Boid boid)
+    {
+        List<Boid> insideRadiusBoids = GetBoidsInsideRadius(boid);
+        if (insideRadiusBoids.Count == 0) return Vector3.zero;
+
+        Vector3 avg = Vector3.zero;
+        foreach (Boid b in insideRadiusBoids)
+        {
+            avg += (boid.transform.position - b.transform.position);
+        }
+        avg /= insideRadiusBoids.Count;
+        return avg.normalized;
+    }
+
+    public Vector3 Direction(Boid boid)
+    {
+        return (target.position - boid.transform.position).normalized;
+    }
+
+    public List<Boid> GetBoidsInsideRadius(Boid boid)
+    {
+        List<Boid> insideRadiusBoids = new List<Boid>();
+
+        foreach (Boid b in boids)
+        {
+            if (Vector3.Distance(boid.transform.position, b.transform.position) < boid.detectionRadious)
+            {
+                insideRadiusBoids.Add(b);
             }
         }
 
-
-        private void OnValidate()
-        {
-            Boid.alignmentWeight = alignmentWeight;
-            Boid.cohesionWeight = cohesionWeight;
-            Boid.separationWeight = separationWeight;
-            Boid.directionWeight = directionWeight;
-        }
-
-        public Vector3 Alignment(Boid boid)
-        {
-            List<Boid> insideRadiusBoids = GetBoidsInsideRadius(boid);
-            if (insideRadiusBoids.Count == 0) return Vector3.zero;
-
-            Vector3 avg = Vector3.zero;
-            foreach (Boid b in insideRadiusBoids)
-            {
-                avg += b.transform.up;
-            }
-
-            avg /= insideRadiusBoids.Count;
-            return avg.normalized;
-        }
-
-        public Vector3 Cohesion(Boid boid)
-        {
-            List<Boid> insideRadiusBoids = GetBoidsInsideRadius(boid);
-            if (insideRadiusBoids.Count == 0) return Vector3.zero;
-
-            Vector3 avg = Vector3.zero;
-            foreach (Boid b in insideRadiusBoids)
-            {
-                avg += b.transform.position;
-            }
-
-            avg /= insideRadiusBoids.Count;
-            return (avg - boid.transform.position).normalized;
-        }
-
-        public Vector3 Separation(Boid boid)
-        {
-            List<Boid> insideRadiusBoids = GetBoidsInsideRadius(boid);
-            if (insideRadiusBoids.Count == 0) return Vector3.zero;
-
-            Vector3 avg = Vector3.zero;
-            foreach (Boid b in insideRadiusBoids)
-            {
-                avg += (boid.transform.position - b.transform.position);
-            }
-
-            avg /= insideRadiusBoids.Count;
-            return avg.normalized;
-        }
-
-        public Vector3 Direction(Boid boid)
-        {
-            return (target.position - boid.transform.position).normalized;
-        }
-
-        public List<Boid> GetBoidsInsideRadius(Boid boid)
-        {
-            List<Boid> insideRadiusBoids = new List<Boid>();
-
-            foreach (Boid b in boids)
-            {
-                if (Vector3.Distance(boid.transform.position, b.transform.position) < boid.detectionRadius)
-                {
-                    insideRadiusBoids.Add(b);
-                }
-            }
-
-            return insideRadiusBoids;
-        }
+        return insideRadiusBoids;
     }
 }
